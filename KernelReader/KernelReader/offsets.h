99aeb896f5e8dea50f1387cc7800944a88da035a
callmelele <140650638+callmelele@users.noreply.github.com>
2026-02-10 23:08:05 +0100

    Fix dwLocalPlayerPawn offset in offsets.h
    Updated the dwLocalPlayerPawn offset value.

--- KernelReader/KernelReader/offsets.h
+++ KernelReader/KernelReader/offsets.h
@@ -4,10 +4,10 @@
 namespace offsets {
     constexpr uintptr_t dwLocalPlayerController = 0x22F0FB8;
     constexpr uintptr_t dwEntityList = 0x24ABF98;
-    constexpr uintptr_t dwLocalPlayerPawn = 0x2066AE0;
+    constexpr uintptr_t dwLocalPlayerPawn = 0x2064ae0;
 
-    constexpr uintptr_t m_hPawn = 0x6C4;              // 1732 decimal
-    constexpr uintptr_t m_iHealth = 0x354;            // 852 decimal
-    constexpr uintptr_t m_pClippingWeapon = 0x3DC0;   // 15808 decimal
-    constexpr uintptr_t m_iClip1 = 0x18D0;            // 6352 decimal
+    constexpr uintptr_t m_hPawn = 0x6C4;              
+    constexpr uintptr_t m_iHealth = 0x354;            
+    constexpr uintptr_t m_pClippingWeapon = 0x3DC0;   
+    constexpr uintptr_t m_iClip1 = 0x18D0;            
 }
