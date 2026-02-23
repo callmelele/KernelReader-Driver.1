3ebfa05b9d3c6fa2cf58d4fad479829d6bf837f6
callmelele <140650638+callmelele@users.noreply.github.com>
2026-02-23 09:26:12 +0100

    Update config.h with additional settings
    Added new configuration options for snaplines, health bar, triggerbot, and other settings.

--- config.h
+++ config.h
@@ -7,8 +7,13 @@ namespace config {
     inline bool g_showSkeleton = true;
     inline bool g_showHeadDot = true;
     inline bool g_showFOV = false;
+    inline bool g_showSnaplines = true;
+    inline bool g_showHealthBar = true;
+    
 
     inline bool g_aimbotEnabled = true;
+    inline bool g_triggerbotAimbotOnly = true;
+    inline bool g_triggerbotEnabled = false;
     inline bool g_menu = true;
 
     inline float thickiness = 2.5f;
@@ -16,8 +21,20 @@ namespace config {
     inline float smoothing = 1.5f;
     inline float g_fov = 100.0f;
     inline int opacity = 255;
+	inline float g_reaction = 100;
+	inline float g_shotInterval = 200;
 
     inline ImU32 color = IM_COL32(255, 255, 255, 255);
     inline ImU32 Hcolor = IM_COL32(255, 255, 255, 255);
     inline ImU32 FOVCOLOR = IM_COL32(255, 0, 0, 255);
+    inline ImU32 SnapLineColor = IM_COL32(255, 255, 255, 155);
+
+    //misc
+    inline bool g_enemiesOnly = true;
+    inline bool g_visibleOnly = true;
+    inline bool g_checkAlive = true;
+    inline bool g_snapLinesBottom = true;
+    inline bool g_showBoxes = true;
+    inline bool g_dynamicThickness = true;
+    inline bool g_hardware = true;
 }
