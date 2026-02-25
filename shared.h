adafde8b50ec8b7c24da5fceed72895cf22fe3f0
callmelele <140650638+callmelele@users.noreply.github.com>
2026-02-25 22:55:40 +0100

    Update shared.h

--- shared.h
+++ shared.h
@@ -7,7 +7,6 @@
 #include <winioctl.h>
 #endif
 
-// The unique code to talk to your driver
 #define IOCTL_COMMAND_HANDLER CTL_CODE(FILE_DEVICE_UNKNOWN, 0x800, METHOD_BUFFERED, FILE_ANY_ACCESS)
 
 typedef enum _COMMAND_TYPE {
@@ -17,11 +16,10 @@ typedef enum _COMMAND_TYPE {
     COMMAND_GET_BASE_ADDRESS
 } COMMAND_TYPE;
 
-// This is your unified request struct
 typedef struct _KERNEL_COMMAND_REQUEST {
     unsigned long ProcessId;
     unsigned __int64 Address;
     void* Buffer;
     unsigned __int64 Size;
-    COMMAND_TYPE Command; // Tells the driver WHAT to do
+    COMMAND_TYPE Command; 
 } KERNEL_COMMAND_REQUEST, * PKERNEL_COMMAND_REQUEST;
